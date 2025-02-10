f1a382e9699427776b2df57fe673a2a4cd61e6b6
Andrew Wallace <aswallace92@gmail.com>
2025-02-10 16:00:32 -0600

    make validator abstract

--- src/edu/jhu/apl/patterns_class/dom/Validator.java
+++ src/edu/jhu/apl/patterns_class/dom/Validator.java
@@ -10,7 +10,7 @@ import org.w3c.dom.UserDataHandler;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
-public class Validator<T extends edu.jhu.apl.patterns_class.dom.replacement.Node> extends edu.jhu.apl.patterns_class.dom.Node{
+public abstract class Validator<T extends edu.jhu.apl.patterns_class.dom.replacement.Node> extends edu.jhu.apl.patterns_class.dom.Node{
 
     T node;
 
